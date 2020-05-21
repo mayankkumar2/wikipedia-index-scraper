ed89880d8a2a3bb50da6c52a8ef69da0687e0859
Mayank Kumar <mayankkumar15@acm.org>
2020-05-21 11:42:05 +0530

    Sets Package Added

--- pkgs/sets/sets.go
+++ pkgs/sets/sets.go
@@ -1,1 +1,36 @@
 package sets
+
+
+var exists = struct{}{}
+
+type Set struct {
+	m map[string]struct{}
+}
+
+func NewSet() *Set {
+	s := &Set{}
+	s.m = make(map[string]struct{})
+	return s
+}
+
+func (s *Set) Add(value string) {
+	s.m[value] = exists
+}
+
+func (s *Set) Remove(value string) {
+	delete(s.m, value)
+}
+
+func (s *Set) Contains(value string) bool {
+	_, c := s.m[value]
+	return c
+}
+func (s *Set) Slice() []string {
+	c := make([]string,len(s.m))
+	cnt := 0
+	for i, _ := range s.m {
+		c[cnt] = i
+		cnt++
+	}
+	return c
+}
