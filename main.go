34aa537bf222aab1810dfe4d29cdcd56e6b70c06
Mayank Kumar <mayankkumar15@acm.org>
2020-05-21 11:29:02 +0530

    Making Graph Deps

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"wikipediaScraper/pkgs/sets"
 	"wikipediaScraper/pkgs/utils"
 	"os"
+	"regexp"
+	"bufio"
+	"strconv"
+	"encoding/json"
 )
 
 func check(e error) {
@@ -13,7 +18,7 @@ func check(e error) {
 	}
 }
 
-func main() {
+func scr() {
 	index := utils.MakePermutations()
 	f, err := os.Create("./data")
 	check(err)
@@ -26,4 +31,61 @@ func main() {
 		c.Visit(fmt.Sprintf("https://en.wikipedia.org/wiki/Special:AllPages/%s",v))
 		fmt.Println("Wrote file for : ",v)
 	}
+}
+func GetData() []string {
+	data := make([]string,340000)
+	inFile, err := os.Open("data")
+	if err != nil {
+		fmt.Println(err.Error() + `: ` + "data")
+		return []string{}
+	}
+	defer inFile.Close()
+	i := 0
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+		data[i] = (scanner.Text())
+		i++
+	}
+	return (data)
+}
+
+
+func indexOf(element string, data []string) (int) {
+	for k, v := range data {
+		if element == v {
+			return k
+		}
+	}
+	return -1
+}
+func main() {
+	d := GetData()
+	c := colly.NewCollector()
+	fmt.Println(d[:1])
+	s := make(map[string] []string)
+	var se *sets.Set
+	c.OnHTML("#bodyContent a[href]", func(e *colly.HTMLElement) {
+		m, err := regexp.MatchString("^/wiki/.*",e.Attr("href"))
+		check(err)
+
+		if m {
+			val := indexOf("https://en.wikipedia.org"+e.Attr("href"),d)
+			if val != -1 {
+				se.Add(strconv.Itoa(val))
+			}
+		}
+	})
+	for i,v := range d {
+		se = sets.NewSet()
+		fmt.Printf("[%d] Visting %s ...\n", i, v)
+		c.Visit(v)
+		s[strconv.Itoa(i)] = se.Slice()
+		fmt.Println(se.Slice())
+		break
+	}
+	f, err := os.Create("graph")
+	check(err)
+	defer f.Close()
+	b, _ := json.Marshal(s)
+	f.Write(b)
 }
